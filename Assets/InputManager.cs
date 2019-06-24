1c4cdc0703eb01e3268e1642674219558e05eaca
SeverinM <severinmichaut@gmail.com>
2019-06-24 21:30:58 +0200

    Input handle

---
+++ Assets/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    float speedCam;
+
+    [SerializeField]
+    Transform cam;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            cam.position += Vector3.left * speedCam * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            cam.position += Vector3.right * speedCam * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            cam.position += Vector3.up * speedCam * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cam.position += Vector3.down * speedCam * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
